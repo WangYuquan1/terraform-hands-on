af88992cf6251214ee300cf51ebdc64605ea8a86
gerray <wyq0213@gmail.com>
2022-03-07 15:13:46 +0900

    terraform gcp example

---
+++ learn-terraform-gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/Users/wang_yuquan/Downloads/root-slate-341702-37e734dc49cb.json")
+
+  project = "root-slate-341702"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
